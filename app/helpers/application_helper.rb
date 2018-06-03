34ff41cf9e58f9321c5b9cca3251b5f4c3bdecb0
ChasobaAlpacanist <chasobakun@gmail.com>
2018-06-03 17:24:08 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #引数に=をつけることで初期値の設定ができる。
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
